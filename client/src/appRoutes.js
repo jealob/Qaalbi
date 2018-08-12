d77d507334dede8e824e5694db5eeda2bff71685
Saad1987 <saad.mourtada@gmail.com>
2018-08-11 21:38:54 -0500

    passing props/auth to Dashboard

--- client/src/appRoutes.js
+++ client/src/appRoutes.js
@@ -27,9 +27,9 @@ export const appRoutes = () => {
     <div>
         <Route path="/" render={(props) => <Nav auth={auth} {...props} />} />
         <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
+        <Route exact path="/dashboard" render={(props) => <Dashboard auth={auth} {...props} />}  />
         <Route exact path="/login" component={Login} />
         <Route exact path="/addevent" component={AddEvent} />
-        <Route exact path="/dashboard" component={Dashboard} />
         <Route path="/callback" render={(props) => {
             handleAuthentication(props);
             return <Callback {...props} /> 
