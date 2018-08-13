7a71fdb2eb824710daf0c989fbc34e29cb79b295
Saad1987 <saad.mourtada@gmail.com>
2018-08-13 18:33:49 -0500

    changed the Callback URL in order to deploy an MVP into heroku

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'kratos7.auth0.com',
   clientId: 'MHaIpREd7GZ1UD4qnwgJENDnCraWWsL7',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://qaalbievents.herokuapp.com/callback'
 }
