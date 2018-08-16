d18e537275808031910a06720a0d5d078f998aff
hodanroble <hodanroble@rochesterstemacademy.org>
2018-08-15 21:43:07 -0500

    added to event calculator form

--- client/src/components/ExpenseCalculator/ExpenseCalculator.js
+++ client/src/components/ExpenseCalculator/ExpenseCalculator.js
@@ -64,68 +64,127 @@ const ExpenseCalculator = (props) => {
         </select>
       </div>
     </div>
-    
-    {/* <!-- Multiple Checkboxes --> */}
-    <div class="form-group">
-      <label class="col-md-4 control-label" for="professional_services">Professional Services</label>
-      <div class="col-md-4">
-      <div class="checkbox">
-        <label for="professional_services-0">
-          <input type="checkbox" name="professional_services"onclick="calculateTotal()"  id="professional_services-0" value="photographer"/>
-          Photographer
-        </label>
+    <label class="col-md-4 control-label" id="header" for="rentals">Rentals</label>
+        <div class="col-md-5">
+          <label class="checkbox-inline" for="rentals-0">
+            <input type="checkbox" name="rentals" id="rentals-0" value="Event Accessories"/> Event Accessories
+          </label>
+          <label class="checkbox-inline" for="rentals-1">
+            <input type="checkbox" name="rentals" id="rentals-1" value="Lighting"/> Lighting
+          </label>
+          <label class="checkbox-inline" for="rentals-2">
+            <input type="checkbox" name="rentals" id="rentals-2" value="Tent"/> Tent
+          </label>
+          <label class="checkbox-inline" for="rentals-3">
+            <input type="checkbox" name="rentals" id="rentals-3" value="Tables"/> Tables
+          </label>
+          <label class="checkbox-inline" for="rentals-4">
+            <input type="checkbox" name="rentals" id="rentals-4" value="Chair"/> Chair
+          </label>
+          <label class="checkbox-inline" for="rentals-5">
+            <input type="checkbox" name="rentals" id="rentals-5" value="Photo Booth"/> Photo Booth
+          </label>
+          <label class="checkbox-inline" for="rentals-6">
+            <input type="checkbox" name="rentals" id="rentals-6" value="Transportation"/> Transportation
+          </label>
         </div>
-      <div class="checkbox"/>
-        <label for="professional_services-1">
-          <input type="checkbox" name="professional_services"onclick="calculateTotal()" id="professional_services-1" value="dj"/>
-          DJ
-        </label>
-        </div>
-      <div class="checkbox">
-        <label for="professional_services-2">
-          <input type="checkbox" name="professional_services"onclick="calculateTotal()" id="professional_services-2" value="hairdresser"/>
-          Hair Dresser
-        </label>
-        </div>
-      <div class="checkbox">
-        <label for="professional_services-3">
-          <input type="checkbox" name="professional_services"onclick="calculateTotal()" id="professional_services-3" value="makeupartist"/>
-          Makeup Artist
-        </label>
-        </div>
-      </div>
-    
-    
-    {/* <!-- Multiple Checkboxes --> */}
-    <div class="form-group">
-      <label class="col-md-4 control-label" for="add_services">Additional Services</label>
-      <div class="col-md-4">
-      <div class="checkbox">
-        <label for="add_services-0">
-          <input type="checkbox" name="add_services"onclick="calculateTotal()" id="add_services-0" value="openbar"/>
-          Open Bar
-        </label>
-        </div>
-      <div class="checkbox">
-        <label for="add_services-1">
-          <input type="checkbox" name="add_services"onclick="calculateTotal()" id="add_services-1" value="eventplanner"/>
-          Party/Event Planner
-        </label>
-        </div>
-      <div class="checkbox">
-        <label for="add_services-2">
-          <input type="checkbox" name="add_services" onclick="calculateTotal()"id="add_services-2" value="invitations"/>
-          Invitations
-        </label>
-        </div>
-      <div class="checkbox">
-        <label for="add_services-3">
-          <input type="checkbox" name="add_services"onclick="calculateTotal()" id="add_services-3" value="decorations"/>
-          Decorations
-        </label>
-        </div>
-      </div>
+
+
+{/* <!-- Multiple Checkboxes (inline) --> */}
+<div class="form-group">
+  <label class="col-md-4 control-label" id="header" for="decorations">Decorations</label>
+  <div class="col-md-5">
+    <label class="checkbox-inline" for="decorations-0">
+      <input type="checkbox" name="decorations" id="decorations-0" onclick="calculateTotal()" value="Bouquets"/> Bouquets
+    </label>
+    <label class="checkbox-inline" for="decorations-1">
+      <input type="checkbox" name="decorations" id="decorations-1" onclick="calculateTotal()" value="Boutonnieres_Corsages"/> Boutonnieres/ Corsages
+    </label>
+    <label class="checkbox-inline" for="decorations-2">
+      <input type="checkbox" name="decorations" id="decorations-2"onclick="calculateTotal()" value="Event Decorations"/> Flowers
+    </label>
+    <label class="checkbox-inline" for="decorations-3">
+      <input type="checkbox" name="decorations" id="decorations-3" onclick="calculateTotal()" value="flowers"/> Event Flowers &amp; Arrangements
+    </label>
+    <label class="checkbox-inline" for="decorations-4">
+      <input type="checkbox" name="decorations" id="decorations-4" onclick="calculateTotal()" value="arrangements"/> Event Table Centerpieces
+    </label>
+    <label class="checkbox-inline" for="decorations-5">
+      <input type="checkbox" name="decorations" id="decorations-5" onclick="calculateTotal()" value="centerpieces"/> Other Decor
+    </label>
+  </div>
+</div>
+
+{/* <!-- Multiple Checkboxes --> */}
+<div class="form-group">
+  <label class="col-md-4 control-label" id="header" for="gift_favors">Gift/Favors</label>
+  <div class="col-md-4">
+    <div class="checkbox">
+      <label for="gift_favors-0">
+        <input type="checkbox" name="gift_favors" id="gift_favors-0" onclick="calculateTotal()" value="Gifts for Attendees"/> Gifts for Attendees
+      </label>
     </div>
+    <div class="checkbox">
+      <label for="gift_favors-1">
+        <input type="checkbox" name="gift_favors" id="gift_favors-1" onclick="calculateTotal()" value="Favors"/> Favors
+      </label>
+    </div>
+    <div class="checkbox">
+      <label for="gift_favors-2">
+        <input type="checkbox" name="gift_favors" id="gift_favors-2" onclick="calculateTotal()" value="Programs"/> Programs
+      </label>
+    </div>
+    <div class="checkbox">
+      <label for="gift_favors-3">
+        <input type="checkbox" name="gift_favors" id="gift_favors-3" onclick="calculateTotal()" value="Guest Book"/> Guest Book
+      </label>
+    </div>
+    <div class="checkbox">
+      <label for="gift_favors-4">
+        <input type="checkbox" name="gift_favors" id="gift_favors-4" onclick="calculateTotal()" value="Invitations/Reply Cards"/> Invitations/Reply Cards
+      </label>
+    </div>
+    <div class="checkbox">
+      <label for="gift_favors-5">
+        <input type="checkbox" name="gift_favors" id="gift_favors-5" onclick="calculateTotal()" value="Menus"/> Menus
+      </label>
+    </div>
+    <div class="checkbox">
+      <label for="gift_favors-6">
+        <input type="checkbox" name="gift_favors" id="gift_favors-6" onclick="calculateTotal()" value="Place Cards"/> Place Cards
+      </label>
+    </div>
+    <div class="checkbox">
+      <label for="gift_favors-7">
+        <input type="checkbox" name="gift_favors" id="gift_favors-7" onclick="calculateTotal()" value="Thank You Cards"/> Thank You Cards
+      </label>
+    </div>
+  </div>
+</div>
+
+{/* <!-- Multiple Checkboxes (inline) --> */}
+<div class="form-group">
+  <label class="col-md-4 control-label" id="header" for="entertainment">Entertainment</label>
+  <div class="col-md-4">
+    <label class="checkbox-inline" for="entertainment-0">
+      <input type="checkbox" name="entertainment" id="entertainment-0" onclick="calculateTotal()" value="DJ"/> DJ
+    </label>
+    <label class="checkbox-inline" for="entertainment-1">
+      <input type="checkbox" name="entertainment" id="entertainment-1" onclick="calculateTotal()" value="MC"/> MC
+    </label>
+    <label class="checkbox-inline" for="entertainment-2">
+      <input type="checkbox" name="entertainment" id="entertainment-2" onclick="calculateTotal()" value="Videographer"/> Videographer
+    </label>
+    <label class="checkbox-inline" for="entertainment-3">
+      <input type="checkbox" name="entertainment" id="entertainment-3" onclick="calculateTotal()" value="Photographer"/> Photographer
+    </label>
+    <label class="checkbox-inline" for="entertainment-4">
+      <input type="checkbox" name="entertainment" id="entertainment-4" onclick="calculateTotal()" value="Live Band"/> Live Band
+    </label>
+    <label class="checkbox-inline" for="entertainment-5">
+      <input type="checkbox" name="entertainment" id="entertainment-5" onclick="calculateTotal()" value="Musicians"/> Musicians
+    </label>
+  </div>
 
     <div id="totalPrice">
 
@@ -136,10 +195,11 @@ const ExpenseCalculator = (props) => {
       <div class="col-md-4">
         <button id="submit_button" name="submit_button" class="btn btn-warning">Submit</button>
       </div>
+      </div>
     </div>
-    
     </fieldset>
     </form>
+
     
     );
   };
