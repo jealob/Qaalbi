fe4d1aec4e12414119109f95f425474ddff70f83
ileysosman <ileys.osmann@gmail.com>
2018-08-25 01:58:27 -0500

    updated form

--- client/src/components/ApptForm/ApptForm.js
+++ client/src/components/ApptForm/ApptForm.js
@@ -1,70 +1,40 @@
 import React from "react";
 import "./AppForm.css";
-const AppForm = props => (
-    <div className="container">
-        <div className="row">
-            <div className="col-md-6">
-                <div className="well-block">
-                    <div className="well-title">
-                        <h2>Questions? Book an Appointment</h2>
-                    </div>
-                    <form>
-                        {/* Form start */}
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label className="control-label" htmlFor="name">Name</label>
-                                    <input id="name" name="name" type="text" placeholder="Name" className="form-control input-md" />
-                                </div>
-                            </div>
-                            {/* Text input*/}
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label className="control-label" htmlFor="email">Email</label>
-                                    <input id="email" name="email" type="text" placeholder="E-Mail" className="form-control input-md" />
-                                </div>
-                            </div>
-                            {/* Text input*/}
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label className="control-label" htmlFor="date">Preferred Date</label>
-                                    <input id="date" name="date" type="text" placeholder="Preferred Date" className="form-control input-md" />
-                                </div>
-                            </div>
-                            {/* Select Basic */}
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label className="control-label" htmlFor="time">Preferred Time</label>
-                                    <select id="time" name="time" className="form-control">
-                                        <option value="8:00 to 9:00">8:00 to 9:00</option>
-                                        <option value="9:00 to 10:00">9:00 to 10:00</option>
-                                        <option value="10:00 to 1:00">10:00 to 1:00</option>
-                                    </select>
-                                </div>
-                            </div>
-                            {/* Select Basic */}
-                            <div className="col-md-12">
-                                <div className="form-group">
-                                    <label className="control-label" htmlFor="appointmentfor">Appointment For</label>
-                                    <select id="appointmentfor" name="appointmentfor" className="form-control">
-                                        <option value="Service#1">Organizational Event</option>
-                                        <option value="Service#2">Personal Event</option>
-                                        <option value="Service#3">Wedding</option>
-                                        <option value="Service#4">Other</option>
-                                    </select>
-                                </div>
-                            </div>
-                            {/* Button */}
-                            <div className="col-md-12">
-                                <div className="form-group">
-                                    <button id="singlebutton" name="singlebutton" className="btn btn-default">Make An Appointment</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                    {/* form end */}
+
+const AppForm = props => {
+      return (
+  
+        <div className="landing-page"><div className="form-appointment"><div className="wpcf7" id="wpcf7-f560-p590-o1"><form action="/landing-page-template-do-not-delete/#wpcf7-f560-p590-o1" method="post" className="wpcf7-form" noValidate="novalidate" _lpchecked={1}>
+                <div style={{display: 'none'}}>
+                  <input type="hidden" name="_wpcf7" defaultValue={560} />
+                  <input type="hidden" name="_wpcf7_version" defaultValue="3.5" />
+                  <input type="hidden" name="_wpcf7_locale" defaultValue />
+                  <input type="hidden" name="_wpcf7_unit_tag" defaultValue="wpcf7-f560-p590-o1" />
+                  <input type="hidden" name="_wpnonce" defaultValue="dbb28877d5" />
                 </div>
-            </div>  </div>
-    </div>
-);
+                <div className="group">
+                  <div style={{width: '48%', float: 'left'}}>
+                    <span className="wpcf7-form-control-wrap text-680"><input type="text" name="text-680" defaultValue size={45} className="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" placeholder="Name" /></span><br />
+                    <span className="wpcf7-form-control-wrap email-680"><input type="email" name="email-680" defaultValue size={45} className="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" placeholder="Email" /></span><br />
+                    <span className="wpcf7-form-control-wrap tel-630"><input type="tel" name="tel-630" defaultValue size={45} className="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel" aria-required="true" placeholder="Phone" /></span><br />
+                    <span className="wpcf7-form-control-wrap textarea-398"><textarea name="textarea-398" cols={40} rows={10} className="wpcf7-form-control wpcf7-textarea" placeholder="Special notes, concerns, or requirements" defaultValue={""} /></span></div>
+                  <div style={{width: '48%', float: 'right'}}>
+                    <h4>What is the best way to reach you?</h4>
+                    <p><span className="wpcf7-form-control-wrap radio-98"><span className="wpcf7-form-control wpcf7-radio"><span className="wpcf7-list-item"><label><input type="radio" name="radio-98" defaultValue="Phone" />&nbsp;<span className="wpcf7-list-item-label">Phone</span></label></span><span className="wpcf7-list-item"><label><input type="radio" name="radio-98" defaultValue="Email" />&nbsp;<span className="wpcf7-list-item-label">Email</span></label></span></span></span></p>
+                    <h4>Days of the week you are available for appointment:</h4>
+                    <p><span className="wpcf7-form-control-wrap checkbox-465"><span className="wpcf7-form-control wpcf7-checkbox"><span className="wpcf7-list-item"><label><input type="checkbox" name="checkbox-465[]" defaultValue="Monday" />&nbsp;<span className="wpcf7-list-item-label">Monday</span></label></span><span className="wpcf7-list-item"><label><input type="checkbox" name="checkbox-465[]" defaultValue="Tuesday" />&nbsp;<span className="wpcf7-list-item-label">Tuesday</span></label></span><span className="wpcf7-list-item"><label><input type="checkbox" name="checkbox-465[]" defaultValue="Wednesday" />&nbsp;<span className="wpcf7-list-item-label">Wednesday</span></label></span><span className="wpcf7-list-item"><label><input type="checkbox" name="checkbox-465[]" defaultValue="Thursday" />&nbsp;<span className="wpcf7-list-item-label">Thursday</span></label></span><span className="wpcf7-list-item"><label><input type="checkbox" name="checkbox-465[]" defaultValue="Friday" />&nbsp;<span className="wpcf7-list-item-label">Friday</span></label></span></span></span></p>
+                    <h4>Best time of day for your appointment:</h4>
+                    <p><span className="wpcf7-form-control-wrap checkbox-246"><span className="wpcf7-form-control wpcf7-checkbox"><span className="wpcf7-list-item"><label><input type="checkbox" name="checkbox-246[]" defaultValue="Morning" />&nbsp;<span className="wpcf7-list-item-label">Morning</span></label></span><span className="wpcf7-list-item"><label><input type="checkbox" name="checkbox-246[]" defaultValue="Afternoon" />&nbsp;<span className="wpcf7-list-item-label">Afternoon</span></label></span></span></span></p></div>
+                </div>
+                <div style={{textAlign: 'center', paddingTop: '2em', borderTop: '1px solid rgb(153, 202, 129)', marginTop: '1em'}}><input type="submit" defaultValue="Request My Appointment" className="wpcf7-form-control wpcf7-submit" /><img className="ajax-loader" src="http://www.professionalaudiologicalservices.com/wp-content/plugins/contact-form-7/images/ajax-loader.gif" alt="Sending ..." style={{visibility: 'hidden'}} /></div>
+                <div className="wpcf7-response-output wpcf7-display-none" />
+                </form>
+                </div>
+                </div>
+                </div>
+      
+    
+      );
+    };
+    
 export default AppForm;
--- client/src/components/ApptForm/index.js
+++ client/src/components/ApptForm/index.js
@@ -1,1 +1,1 @@
-export { default } from "./ApptForm";
+export { default } from "./AppForm";
