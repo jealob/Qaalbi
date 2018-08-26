120ce50d26789ad6c323ae6324afd7fb676c7797
jealob <jealob@gmail.com>
2018-08-25 20:00:11 -0500

    Updates

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -30,7 +30,7 @@ export class Nav extends Component {
           !isAuthenticated() && (
             <nav id="id" className="navbar navbar-expand nav-content">
               <div className="container">
-                <a className="navbar-brand" href="/">  <img id="logoImg" className='mr-5' src={require("../../images/logo.jpg")} width="150" height="50" alt="logonav"/> </a>
+                <a className="navbar-brand" href="/">  <img id="logoImg" className='mr-5' src={require("../../images/logo.jpg")} width="150" height="50" alt="logonav" /> </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="navbarNav"
                   aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
@@ -62,7 +62,7 @@ export class Nav extends Component {
             <nav id="id" className="navbar navbar-expand nav-content">
               <div className="container">
                 <a className="navbar-brand" href="/" >
-                <img src={require("../../images/logo.jpg")}  className='mr-5'  id="logoImg"  width="150" height="50" alt="logonav"/> </a>
+                  <img src={require("../../images/logo.jpg")} className='mr-5' id="logoImg" width="150" height="50" alt="logonav" /> </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-controls="navbarNav"
                   aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
@@ -102,26 +102,27 @@ export class Nav extends Component {
 export const Footer = () => {
   return (
     <footer className="main-page-footer">
-    <div className="row">
-      <h4 className=" pt-3 col text-center"> Follow Us: 
-         <a href="https://www.twitter.com/" target="_blank">  <img className="m-2 " src={require("../../images/tweet.png")} alt="twitter" /></a> 
-         <a href="https://www.instagram.com/" target="_blank">  <img className="m-2 " src={require("../../images/instagram.png")}  alt="instagram" /></a>
-         <a href="https://www.facebook.com/" target="_blank">  <img className="m-2 " src={require("../../images/facebook.png")}  alt="facebook" /></a>
-         <a href="https://www.googleplus.com/" target="_blank">  <img className="m-2" src={require("../../images/googleplus.png")}  alt="googleplus" /></a>
-      </h4>
-    </div>
-    <div className="row">
-    <ul className="col text-center">
-     <li  className="items mr-3"><a href="https://www.theknot.com/" target="_blank">Wedding Invintation </a></li>
-     <li  className="items mr-3"><a href="https://www.theknot.com/" target="_blank">Wedding Cakes </a></li>
-     <li  className="items mr-3"><a href="https://www.theknot.com/" target="_blank">Wedding Photos </a></li>
-    </ul>
-    <br/>
-    </div> 
-    <div className="row"> 
-    <p className="col text-center"> Copyright &copy; 2018</p>
-   </div>
-    </footer>
+      <div className="row">
+        <div className="col  text-center">
+          {/* <h4 className=" pt-3 col text-center"> Follow Us: </h4> */}
+          <a href="https://www.twitter.com/" target="_blank"><img className="m-2 " src={require("../../images/tweet.png")} alt="twitter" /></a>
+          <a href="https://www.instagram.com/" target="_blank"><img className="m-2 " src={require("../../images/instagram.png")} alt="instagram" /></a>
+          <a href="https://www.facebook.com/" target="_blank"><img className="m-2 " src={require("../../images/facebook.png")} alt="facebook" /></a>
+          <a href="https://www.googleplus.com/" target="_blank"><img className="m-2" src={require("../../images/googleplus.png")} alt="googleplus" /></a>
+        </div>
+      </div >
+      <div className="row">
+        <ul className="col text-center">
+          <li className="items mr-3"><a href="https://www.theknot.com/" target="_blank">Wedding Invintation </a></li>
+          <li className="items mr-3"><a href="https://www.theknot.com/" target="_blank">Wedding Cakes </a></li>
+          <li className="items mr-3"><a href="https://www.theknot.com/" target="_blank">Wedding Photos </a></li>
+        </ul>
+        <br />
+      </div>
+      <div className="row">
+        <p className="col text-center"> Copyright &copy; 2018</p>
+      </div>
+    </footer >
   )
 }
 
