cfe84306511cab37642e1b8fc9d94481048a9e18
jealob <jealob@gmail.com>
2018-08-25 21:05:10 -0500

    Updates

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -80,6 +80,7 @@ export default class Dashboard extends Component {
                                     </div>
                                     <div className=" col-xs-12 col-sm-12 col-md-8 col-lg-8 text-center">
                                         <AddEvent id="events" />
+                                        <ExpenseCalculator />
                                         <AppointmentForm />
                                     </div>
 
